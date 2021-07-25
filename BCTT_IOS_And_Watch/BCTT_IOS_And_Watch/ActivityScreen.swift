ecd5d602cdb29b9407fb30796186fabcd232bdff
Joshua Kent <jkent40@gatech.edu>
2021-07-25 16:40:26 -0400

    API calls working

--- BCTT_IOS_And_Watch/BCTT_IOS_And_Watch/ActivityScreen.swift
+++ BCTT_IOS_And_Watch/BCTT_IOS_And_Watch/ActivityScreen.swift
@@ -5,6 +5,11 @@
 //  Created by Joshua Kent on 6/30/21.
 //
 
+//TO DO:
+//1. Get sessionID in newsession call
+//2. Get HR and time data to show on website
+
+
 import SwiftUI
 
 struct ActivityScreen: View {
@@ -16,6 +21,7 @@ struct ActivityScreen: View {
     
     @State var reachable = "No"
     @State var heartRateToDisplay = "0"
+    @State var sessionID = "-1"
     
     //stop watch
     @ObservedObject var stopWatchManager = StopWatchManager()
@@ -48,10 +54,8 @@ struct ActivityScreen: View {
                     Button(action: {
                         self.stopWatchManager.stop()
                         
-                        //implement /api/endsession HERE
-                        
-                        
-                        
+                        //calls function that does endSession API call
+                        self.endSession()
                         
                     }) {
                         TimerButton(label: "End Test", buttonColor: .red)
@@ -69,12 +73,18 @@ struct ActivityScreen: View {
                 Text("\(heartRateToDisplay)").onReceive(timer, perform: { _ in
                     heartRateToDisplay = String(self.model.heartRateFromWatch)
                     
-                    //implement /api/pushdata HERE
-                    
-                    
-                    
-                    
-                    
+                    if stopWatchManager.secondsElapsed % 5 == 0 && stopWatchManager.secondsElapsed != 0 {
+                        //create a timp stamp
+                        let time = [Int(Date().timeIntervalSince1970)]
+
+                        //-1 means there is an error
+                        let hrToSend = [Int(heartRateToDisplay) ?? -1]
+                        
+                        //for testing
+                        //let hrToSend = [Int.random(in: 1..<100)]
+                        
+                        self.pushData(heartRate: hrToSend, timeStamp: time)
+                    }
                 })
                 .font(.system(size: 100))
 
@@ -109,9 +119,94 @@ struct ActivityScreen: View {
         .onAppear(perform: startNewSession)
     }
     
+    //implement /api/newsession HERE
+    //sends patient name to make a new session, recieves a session ID
     func startNewSession() {
         print("User name: \(name)")
-        //implement /api/newsession HERE
+        
+        //prepare data in JSON format
+        let json: [String: Any] = ["name": name]
+    
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        // actual API call
+        let url = URL(string: "http://localhost:3000/api/newsession")!
+        var request = URLRequest(url: url)
+        //request.setValue("\(jsonData.length)", forHTTPHeaderField: "Content-Length")
+        //request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Session ID: \(dataString)")
+                sessionID = dataString
+            }
+            
+//            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+//
+//            if responseJSON == nil { print("Its nill")}
+//
+//            if let responseJSON = responseJSON as? [String: Any] { //changed [String: Any] to String
+//                print("rj: \(responseJSON)")
+//                for key in responseJSON.keys {
+//                   print("\(key)")
+//                }
+//
+//            }
+        }.resume()
+    }
+    
+    //implement /api/pushdata HERE
+    //sends heart rate and time stamp, recieves nothing
+    func pushData(heartRate: [Int], timeStamp: [Int]) {
+        print("Pushing data . . . HR: \(heartRate), time: \(timeStamp)")
+        
+        //prepare data in JSON format
+        let json: [String: Any] = ["data": heartRate, "timestamp": timeStamp, "sessionid": sessionID]
+    
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        // actual API call
+        let url = URL(string: "http://localhost:3000/api/pushdata")!
+        var request = URLRequest(url: url)
+        //request.setValue("\(jsonData.length)", forHTTPHeaderField: "Content-Length")
+        //request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            //Nothing that is returned, so just check for errors
+            if let error = error {
+            print("There was an error: \(error.localizedDescription)")
+            }
+        }.resume()
+    }
+    
+    //implement /api/endsession HERE
+    //sends session id, recieves nothing
+    func endSession() {
+        
+        //prepare data in JSON format
+        let json: [String: Any] = ["sessionid": sessionID]
+    
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        // actual API call
+        let url = URL(string: "http://localhost:3000/api/endsession")!
+        var request = URLRequest(url: url)
+        //request.setValue("\(jsonData.length)", forHTTPHeaderField: "Content-Length")
+        //request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            //Nothing that is returned, so just check for errors
+            if let error = error {
+            print("There was an error: \(error.localizedDescription)")
+            }
+        }.resume()
     }
 }
 
--- BCTT_IOS_And_Watch/BCTT_IOS_And_Watch/StopWatchManager.swift
+++ BCTT_IOS_And_Watch/BCTT_IOS_And_Watch/StopWatchManager.swift
@@ -15,22 +15,22 @@ enum stopWatchMode {
 class StopWatchManager: ObservableObject {
     
     @Published var mode: stopWatchMode = .stopped
-    @Published var secondsElapsed = 0.0
+    @Published var secondsElapsed = 0
     @Published var timeToDisplay = "00:00"  //this is not designed to go into hours since the test should not be longer than 20 minutes
     
     var timer = Timer()
     
     func start() {
         mode = .running
-        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
-            self.secondsElapsed = self.secondsElapsed + 0.1
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            self.secondsElapsed = self.secondsElapsed + 1
             var mins = "\(Int(self.secondsElapsed / 60))"
             
             if mins.count == 1 {
                 mins = "0\(mins)"
             }
             
-            var secs = "\(Int(self.secondsElapsed.truncatingRemainder(dividingBy: 60)))"
+            var secs = "\(Int(self.secondsElapsed % 60))"
             
             if secs.count == 1 {
                 secs = "0\(secs)"
